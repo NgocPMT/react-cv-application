dab314873714a400066cdee9b788f0ce7c3d3cf7
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-07-26 21:39:53 +0700

    implement navbar with styling, adjust cv preview spacing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Navbar from "./Navbar";
 import "../css/App.css";
 
 export default function App() {
@@ -86,154 +87,167 @@ export default function App() {
   }, []);
 
   return (
-    <div className="body-wrapper">
-      <div className="form-container">
-        <Form title={forms[0]} index={0} formIndex={formIndex}>
-          <Field
-            title="Job Title"
-            type="text"
-            id="job-title"
-            value={jobTitle}
-            onChange={handleJobTitle}
-          />
-          <FieldGroup>
+    <>
+      <Navbar />
+      <div className="body-wrapper">
+        <div className="form-container">
+          <Form title={forms[0]} index={0} formIndex={formIndex}>
             <Field
-              title="First Name"
+              title="Job Title"
               type="text"
-              id="first-name"
-              value={firstName}
-              onChange={handleFirstName}
+              id="job-title"
+              value={jobTitle}
+              onChange={handleJobTitle}
             />
+            <FieldGroup>
+              <Field
+                title="First Name"
+                type="text"
+                id="first-name"
+                value={firstName}
+                onChange={handleFirstName}
+              />
+              <Field
+                title="Last Name"
+                type="text"
+                id="last-name"
+                value={lastName}
+                onChange={handleLastName}
+              />
+            </FieldGroup>
+            <FieldGroup>
+              <Field
+                title="Email"
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmail}
+              />
+              <Field
+                title="Phone"
+                type="tel"
+                id="phone"
+                value={phone}
+                onChange={handlePhone}
+              />
+            </FieldGroup>
             <Field
-              title="Last Name"
+              title="Address"
               type="text"
-              id="last-name"
-              value={lastName}
-              onChange={handleLastName}
+              id="address"
+              value={address}
+              onChange={handleAddress}
             />
-          </FieldGroup>
-          <FieldGroup>
-            <Field
-              title="Email"
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmail}
-            />
-            <Field
-              title="Phone"
-              type="tel"
-              id="phone"
-              value={phone}
-              onChange={handlePhone}
-            />
-          </FieldGroup>
-          <Field
-            title="Address"
-            type="text"
-            id="address"
-            value={address}
-            onChange={handleAddress}
-          />
-          <FieldGroup>
-            <Field
-              title="Country"
-              type="text"
-              id="country"
-              value={country}
-              onChange={handleCountry}
-            />
-            <Field
-              title="City"
-              type="text"
-              id="city"
-              value={city}
-              onChange={handleCity}
-            />
-          </FieldGroup>
-        </Form>
-        <div className="buttons">
-          {isMobile && (
-            <button onClick={togglePreview} className="preview-btn">
-              Preview
-            </button>
-          )}
+            <FieldGroup>
+              <Field
+                title="Country"
+                type="text"
+                id="country"
+                value={country}
+                onChange={handleCountry}
+              />
+              <Field
+                title="City"
+                type="text"
+                id="city"
+                value={city}
+                onChange={handleCity}
+              />
+            </FieldGroup>
+          </Form>
+          <div className="buttons">
+            {isMobile && (
+              <button onClick={togglePreview} className="preview-btn">
+                Preview
+              </button>
+            )}
 
-          <div className="button-group">
-            <div>
-              <button
-                disabled={isFirst}
-                className={`${isFirst && "disabled"}`}
-                onClick={handlePrev}
-              >
-                Previous
-              </button>
-              <p>{formIndex > 0 ? forms[formIndex - 1] : ""}</p>
-            </div>
-            <div>
-              <button
-                disabled={isLast}
-                className={`${isLast && "disabled"}`}
-                onClick={handleNext}
-              >
-                Next
-              </button>
-              <p>{formIndex < formCounts ? forms[formIndex + 1] : ""}</p>
+            <div className="button-group">
+              <div>
+                <button
+                  disabled={isFirst}
+                  className={`${isFirst && "disabled"}`}
+                  onClick={handlePrev}
+                >
+                  Previous
+                </button>
+                <p>{formIndex > 0 ? forms[formIndex - 1] : ""}</p>
+              </div>
+              <div>
+                <button
+                  disabled={isLast}
+                  className={`${isLast && "disabled"}`}
+                  onClick={handleNext}
+                >
+                  Next
+                </button>
+                <p>{formIndex < formCounts ? forms[formIndex + 1] : ""}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {isMobile && isPreviewOpen && <div className="preview-backdrop"></div>}
-      {(!isMobile || isPreviewOpen) && (
-        <>
-          <div className="cv-preview">
-            <div className="cv-header">
-              <h1 className="cv-full-name">John Smith</h1>
-              <p className="cv-job-title">IT Project Manager</p>
-            </div>
-            <div className="cv-main">
-              <h2>Experience</h2>
-              <div className="experience">
-                <p>2004/09 - 2006/12</p>
-                <div className="experience-details">
-                  <h3>Senior Project Manager</h3>
-                  <p className="company-name">Seton Hospital, ME</p>
-                  <ul>
-                    <li>
-                      Oversee all major hospital IT projects for 10+ years
-                    </li>
-                    <li>
-                      Oversee all major hospital IT projects for 10+ years
-                    </li>
-                    <li>
-                      Oversee all major hospital IT projects for 10+ years
-                    </li>
-                  </ul>
+        {isMobile && isPreviewOpen && (
+          <div
+            className="preview-backdrop"
+            onClick={togglePreview}
+            role="button"
+            aria-label="Close preview"
+            tabIndex={0}
+          ></div>
+        )}
+        {(!isMobile || isPreviewOpen) && (
+          <>
+            <div className="cv-preview">
+              <div className="cv-header">
+                <h1 className="cv-full-name">John Smith</h1>
+                <p className="cv-job-title">IT Project Manager</p>
+              </div>
+              <div className="cv-main">
+                <h2>Experience</h2>
+                <div className="experience">
+                  <p>2004/09 - 2006/12</p>
+                  <div className="experience-details">
+                    <h3>Senior Project Manager</h3>
+                    <p className="company-name">Seton Hospital, ME</p>
+                    <ul>
+                      <li>
+                        Oversee all major hospital IT projects for 10+ years
+                      </li>
+                      <li>
+                        Oversee all major hospital IT projects for 10+ years
+                      </li>
+                      <li>
+                        Oversee all major hospital IT projects for 10+ years
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <h2>Education</h2>
+                <div className="education">
+                  <p>1999/09 - 2001/05</p>
+                  <div className="education-details">
+                    <h3>Master of Computer Science, University of Maryland</h3>
+                    <ul>
+                      <li>Graduated Summa Cum Laude</li>
+                      <li>
+                        Member of Student Association of Project Management
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-              <h2>Education</h2>
-              <div className="education">
-                <p>1999/09 - 2001/05</p>
-                <div className="education-details">
-                  <h3>Master of Computer Science, University of Maryland</h3>
-                  <ul>
-                    <li>Graduated Summa Cum Laude</li>
-                    <li>Member of Student Association of Project Management</li>
-                  </ul>
-                </div>
+              <div className="personal-info">
+                <p>Address: Portland, ME</p>
+                <p>Phone: 0364933038</p>
+                <p>Email: nathan.pham1505@gmail.com</p>
               </div>
             </div>
-            <div className="personal-info">
-              <p>Address: Portland, ME</p>
-              <p>Phone: 0364933038</p>
-              <p>Email: nathan.pham1505@gmail.com</p>
-            </div>
-          </div>
-          {isMobile && <button onClick={togglePreview}>Close</button>}
-        </>
-      )}
-    </div>
+            {isMobile && <button onClick={togglePreview}>Close</button>}
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "../css/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <header>
+      <p>Simple CV Maker</p>
+      <nav>
+        <button>Download As PDF</button>
+      </nav>
+    </header>
+  );
+}
